82be081d2343781b7b4c609024687ad6686a63f4
Lev <levnevinitsin@gmail.com>
2022-05-24 10:21:00 +0300

    Change map from Google to Yandex

--- source/js/contacts.js
+++ source/js/contacts.js
@@ -1,47 +1,42 @@
-const contacts = document.querySelector(".contacts");
+const contacts = document.querySelector('.contacts');
+
+const FAKE_MAP_SELECTOR = '.contacts__fake-map';
+
+const MAP_CONTAINER_ID = 'map';
+const CENTER_LAT = 59.93879978424407;
+const CENTER_LNG = 30.3228420342727;
+const MARKER_LAT = 59.93867978424407;
+const MARKER_LNG = 30.3229920342727;
+const ZOOM = 16;
+const MARKER_ICON_PATH = 'img/icon_map_marker.svg';
 
 if (contacts) {
   map();
 }
 
-function map() {
-  // Remove map picture
-  const contactsMapContainer = document.querySelector(".contacts__map");
-  const contactsMapPicture = contactsMapContainer.querySelector("picture");
+function map () {
+  const fakeMapElement = document.querySelector(FAKE_MAP_SELECTOR);
 
-  contactsMapPicture.remove();
+  ymaps.ready(init);
 
-  // Add heading for accessibility
-  const contactsList = document.querySelector(".contacts__list");
-  const mapHeading = document.createElement("h3");
-  mapHeading.classList.add("visually-hidden");
-  mapHeading.textContent = "Карта с местоположением магазина на карте по адресу: Санкт-Петербург, ул. Большая Конюшенная 19/8, офис 101."
+  function init() {
+    fakeMapElement.remove();
 
-  contactsList.after(mapHeading);
-
-  // Set size for map container
-  contactsMapContainer.classList.add("contacts__map--api");
-
-  // Map script
-  const script = document.createElement('script');
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB0a5vYLNnFwIjYWbqVfy3JaaApqK55qrM&callback=initMap';
-  script.defer = true;
-
-  document.head.appendChild(script);
-
-  window.initMap = function() {
-    const mishkaShop = {lat: 59.93879978424407, lng: 30.3228420342727};
-    const mishkaMarker = {lat: 59.93867978424407, lng: 30.3229920342727};
-    const map = new google.maps.Map(
-      document.querySelector(".contacts__map"), {
-        center: mishkaShop,
-        zoom: 16.65,
-      });
-    const image = "img/icon_map_marker.svg"
-    const marker = new google.maps.Marker({
-      position: mishkaMarker,
-      map: map,
-      icon: image
+    const myMap = new ymaps.Map(MAP_CONTAINER_ID, {
+      center: [CENTER_LAT, CENTER_LNG],
+      zoom: ZOOM,
     });
-  };
+
+    const marker = new ymaps.Placemark([MARKER_LAT, MARKER_LNG], {
+      hintContent: 'qqq',
+      balloonContent: 'www'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: MARKER_ICON_PATH,
+        iconImageSize: [67, 101],
+        iconImageOffset: [-23, -101]
+    });
+
+    myMap.geoObjects.add(marker);
+  }
 }
